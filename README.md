b4a6d0557f8279d40cc4405028dbc81a8b41e177
Brandon <gillen@Brandons-iMac-2.local>
2023-10-31 22:03:41 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Your looking at the start of a new project
+
+# Day 1
---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("Hey you nosy little shit, what do you think your doing?")
+console.log("You better not break anything.")
+setTimeout(() => {
+    console.log('fr tho like leave my stuff alone')
+  }, "4000"); // fun stuff lol
+
+// Enough fun its time to get serious lol
+
+$(document).ready(function() {
+    $('#glob-headers').load('/glob/headers.html');
+}); // Import global headers
+
+
+// Fun Fact: I Havent done the part below yet :D
+$(document).ready(function() {
+    $('#glob-headers').load('/glob/headers.html');
+}); // Import global head segment
+
+$(document).ready(function() {
+    $('#glob-headers').load('/glob/headers.html');
+}); // Import global footer segment
+// Ill do it tommorow lol
